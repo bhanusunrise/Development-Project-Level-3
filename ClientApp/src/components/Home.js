8e798c1f2441a0caf95fe24806fb460594fbc995
bhanusunrise <pasindubhanukagood@gmail.com>
2024-04-08 22:04:46 +0530

    Re-Commit Messege

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import { Carasoul } from './Carasoul';
-import { CarText } from './CarText';
 export class Home extends Component {
   static displayName = Home.name;
   render() {
     return (
         <div>
             <Carasoul />
-            <CarText />
+            
 
       </div>
     );
